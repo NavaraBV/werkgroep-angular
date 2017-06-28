f57fc151c2321e413157de8793720ee7d094bd4d
Frank Borgonjen <frank.borgonjen@triodos.nl>
2017-06-28 14:46:29 +0200

    Initial setup of forms demo

---
+++ forms/src/app/reactive/reactive.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveComponent } from './reactive.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ReactiveComponent]
+})
+export class ReactiveModule { }
